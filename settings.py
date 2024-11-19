4c6325d1ceb75a07895e927cb8864a5e59e96975
Vyacheslav <vaceslavupcotc@gmail.com>
2024-11-19 23:01:19 +0300

    fix: env

--- settings.py
+++ settings.py
@@ -3,9 +3,12 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_URL: str
     SECRET_KEY: str
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
 
     class Config:
-        env_file = ".env.sample"
+        env_file = ".env"
         env_file_encoding = "utf-8"
 
 settings = Settings()
