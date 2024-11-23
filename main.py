17796c5eb84d64efd9d673a75c42a72d56cddca6
Vyacheslav <vaceslavupcotc@gmail.com>
2024-11-23 19:16:05 +0300

    edit cors2

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from controllers import routers
 
 app = FastAPI()
 
+# Настройка CORS
+origins = ["https://cwnotes.ru"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cwnotes.ru"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
+    expose_headers=["Content-Length"],
+    max_age=3600,
 )
-
 
 for router in routers:
     app.include_router(router, prefix="/api")
